a836f0ea3971433675d2ad292324c3c855700709
Nastia Balenko <balenko.anastasiia@gmail.com>
2021-03-17 14:36:44 +0200

    show right links in nav bar

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,37 +1,46 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import useGetUser from '../hooks/useGetUser';
 
 const Header: React.FC = () => {
+  const { user } = useGetUser();
   return (
     <nav>
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
-        <li>
-          <NavLink to="/sections">Sections</NavLink>
-        </li>
-        <li>
-          <NavLink to="/categories">Categories</NavLink>
-        </li>
-        <li>
-          <NavLink to="/entries">Entries</NavLink>
-        </li>
-        <li>
-          <NavLink to="/charts">Charts</NavLink>
-        </li>
+        {user ? (
+          <>
+            <li>
+              <NavLink to="/sections">Sections</NavLink>
+            </li>
+            <li>
+              <NavLink to="/categories">Categories</NavLink>
+            </li>
+            <li>
+              <NavLink to="/entries">Entries</NavLink>
+            </li>
+            <li>
+              <NavLink to="/charts">Charts</NavLink>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink to="/login">Sign In</NavLink>
+            </li>
+            <li>
+              <NavLink to="/register">Sign up</NavLink>
+            </li>
+          </>
+        )}
       </ul>
-      <ul>
-        <li>
-          <NavLink to="/login">Sign In</NavLink>
-        </li>
-        <li>
-          <NavLink to="/register">Sign up</NavLink>
-        </li>
+      {user && (
         <li>
           <button type="button">Logout</button>
         </li>
-      </ul>
+      )}
     </nav>
   );
 };
